1906d088d61edf2c56f48784a0fde974b1c38c06
mmizani <maryam.mizani@gmail.com>
2021-08-01 09:34:56 -0400

    Add initBinder to trim input Strings of all web requests coming into the CustomerController

--- src/com/demo/springdemo/mvc/CustomerController.java
+++ src/com/demo/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.demo.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("customer")
 public class CustomerController {
 	
+	// Add initBinder to trim input Strings in all web requests coming into this controller
+	@InitBinder
+	public void initBinder(WebDataBinder iDataBinder) {
+		StringTrimmerEditor lStringTrimmerEditor = new StringTrimmerEditor(true); // true: consider Empty as null
+		iDataBinder.registerCustomEditor(String.class, lStringTrimmerEditor);
+	}
+		
 	@RequestMapping("showForm")
 	public String showForm(Model iModel) {
 		iModel.addAttribute("customer", new Customer()); // name, value
@@ -22,6 +32,8 @@ public class CustomerController {
 	public String processForm(@Valid @ModelAttribute("customer") Customer iCustomer,  // perform validation rules on iCustomer
 			BindingResult iBindingResult) // Results of validation will be placed in the iBindingResult
 	{  		
+		
+		System.out.println('|' + iCustomer.getLastName() + '|');
 		if(iBindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
